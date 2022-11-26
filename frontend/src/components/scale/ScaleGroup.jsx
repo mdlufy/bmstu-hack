8ba44d39df3b0994d867d3b442b83eea715ea6c0
mdlufy <german.panov.2018@mail.ru>
2022-11-26 17:32:10 +0300

    feat: zoom and scaling

--- frontend/src/components/scale/ScaleGroup.jsx
+++ frontend/src/components/scale/ScaleGroup.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import Bmstu from "../../objects/bmstu";
+import canvas from "../MainCanvas/MainCanvas";
 import "./styles.css";
 
 function ScaleGroup({ increase, decrease }) {
+    function increase() {
+        Bmstu.scalePlus();
+        canvas.renderAll();
+
+    }
+
+    function decrease() {
+        Bmstu.scaleMinus();
+        canvas.renderAll();
+    }
+
     return (
         <div className="scale">
             <span>
-                <button className="increase" onClick={increase}>
+                <button className="increase" onClick={() => increase()}>
                     +
                 </button>
-                <button className="decrease" onClick={decrease}>
+                <button className="decrease" onClick={() => decrease()}>
                     -
                 </button>
             </span>
--- frontend/src/objects/bmstu.js
+++ frontend/src/objects/bmstu.js
@@ -21,9 +21,34 @@ export default class Bmstu {
         this.group.addWithUpdate(rightArc);
 
         canvas.add(this.group);
+
+        canvas.on("mouse:wheel", function (opt) {
+            var delta = opt.e.deltaY;
+            var zoom = canvas.getZoom();
+            zoom *= 0.999 ** delta;
+            if (zoom > 20) zoom = 20;
+            if (zoom < 0.01) zoom = 0.01;
+            canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+            opt.e.preventDefault();
+            opt.e.stopPropagation();
+        });
     }
 
     static add(obj) {
         this.group.addWithUpdate(obj);
+    }
+
+    static scaleMinus() {
+        this.group.set({
+            scaleY: this.group.scaleX * 0.9,
+            scaleX: this.group.scaleX * 0.9,
+        });
+    }
+
+    static scalePlus() {
+        this.group.set({
+            scaleY: this.group.scaleX * 1.1,
+            scaleX: this.group.scaleX * 1.1,
+        });
     }
 }
