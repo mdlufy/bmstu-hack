e6c37f68e2064a3f3843ae8ac3f1018a93a0e1b7
Vladislav Tkachenko <webkadiz@gmail.com>
2022-11-27 17:28:18 +0300

    work

--- frontend/src/components/Signin.js
+++ frontend/src/components/Signin.js
@@ -1,16 +1,21 @@
-import { Button, Form, Input, message, Modal, } from "antd"
+import { Button, Form, Input, message, Modal } from "antd"
 import axios from "axios"
 import { useDispatch } from "react-redux"
 import { setTokenAction } from "../slices/user"
-import {useState} from "react";
-import styled from "styled-components";
+import { useState } from "react"
+import styled from "styled-components"
+import { useForm } from "antd/es/form/Form"
 
 export default function Signin() {
     const dispatch = useDispatch()
+    const [form] = useForm()
 
     const onFinish = () => {
-        axios("/signin")
-            .then(() => {
+        axios
+            .post("/login", form.getFieldsValue())
+            .then(({ data }) => {
+                console.log(data)
+                dispatch(setTokenAction(data.token))
                 message.success("Вход выполнен")
             })
             .catch(() => {
@@ -21,54 +26,65 @@ export default function Signin() {
 
     const onFinishFailed = () => {}
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false)
 
     const showModal = () => {
-        setIsModalOpen(true);
-    };
+        setIsModalOpen(true)
+    }
 
     const handleOk = () => {
-        setIsModalOpen(false);
-    };
+        setIsModalOpen(false)
+    }
 
     const handleCancel = () => {
-        setIsModalOpen(false);
-    };
+        setIsModalOpen(false)
+    }
 
     return (
         <Container>
-            <Button className={'auth-button'} type="primary" onClick={showModal}>
-                Open Modal
+            <Button
+                className={"auth-button"}
+                type="primary"
+                onClick={showModal}
+            >
+                Войти
             </Button>
-            <Modal title="Авторизация" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <Form
-            name="basic"
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <Form.Item
-                label="Логин"
-                name="login"
-                rules={[{ required: true, message: "Заполните поле" }]}
+            <Modal
+                title="Авторизация"
+                open={isModalOpen}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                footer={null}
             >
-                <Input size="large" />
-            </Form.Item>
+                <Form
+                    form={form}
+                    name="basic"
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="Логин"
+                        name="name"
+                        rules={[{ required: true, message: "Заполните поле" }]}
+                    >
+                        <Input size="large" />
+                    </Form.Item>
 
-            <Form.Item
-                label="Пароль"
-                name="password"
-                rules={[{ required: true, message: "Заполните поле" }]}
-            >
-                <Input.Password />
-            </Form.Item>
+                    <Form.Item
+                        label="Пароль"
+                        name="password"
+                        rules={[{ required: true, message: "Заполните поле" }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
 
-            <Form.Item>
-                <Button type="primary" htmlType="submit">
-                    Войти
-                </Button>
-            </Form.Item>
-        </Form>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            Войти
+                        </Button>
+                    </Form.Item>
+                </Form>
             </Modal>
         </Container>
     )
